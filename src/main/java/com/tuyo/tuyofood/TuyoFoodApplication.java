b372c6db9adcf9889ca00a95bf3fb468d7ec7c0a
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-25 11:49:30 -0300

    #commit - 1

---
+++ src/main/java/com/tuyo/tuyofood/TuyoFoodApplication.java
@@ -0,0 +1,13 @@
+package com.tuyo.tuyofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuyoFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuyoFoodApplication.class, args);
+	}
+
+}
