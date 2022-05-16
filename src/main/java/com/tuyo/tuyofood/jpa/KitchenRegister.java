16ff44719e54867117686d0db54577ee84544e16
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-16 16:38:35 -0300

    #commit 1 Spring_Data_JPA - Update

--- src/main/java/com/tuyo/tuyofood/jpa/KitchenRegister.java
+++ src/main/java/com/tuyo/tuyofood/jpa/KitchenRegister.java
@@ -11,36 +11,20 @@ import java.util.List;
 @Component
 public class KitchenRegister {
 
-   /* Interface EntityManager: gerencia o contexto de persitência de dados.
-   * @PersistenceContext: é a anotação responsável em injetar a interface EntityManager.
-   * Dessa forma, é possível salvar objetos no database, fazer consultas e etc */
     @PersistenceContext
     private EntityManager manager;
 
-    /* Método para listar a lista de Kitchen:
-    * 1. Usando o JPQL para fazer consultas em objetos: "from Kitchen".
-    * 2. Utilizando "from Kitchen" significa que quero o objeto Kitchen inteiro em minha listagem.
-    * 3. O retorno é realizado em "Kitchen.class" */
     public List<Kitchen> listar() {
         return manager.createQuery("from Kitchen", Kitchen.class)
                 .getResultList();
     }
 
-    /* Método para buscar por ID: Só é passar no parâmetro () o tipo de entidade: Kitchen.class e o id.*/
     public Kitchen buscar(Long id) {
         return manager.find(Kitchen.class, id);
     }
 
-    /* A anotação @Transactional = representa a transação que precisa ser feita no database.
-    *  1. Inserção direta no database sem JPA: insert into kitchen (nome) values (kitchen = tabela e (nome)= seriam as colunas)
-    *  2. Com JPA: return manager.merge(entity) = colocando a entity, kitchen, no contexto de persistência.
-    *  3. return = representa o retorno da instância persistida.
-    *  4. O método "merge" não altera a instância no parâmetro: quando uma nova cozinha é incluída, um novo "id" é atribuído
-    * no database que está configurado com "auto-incremento". Esse "id" não estará atribuído na instância de Kitchen em
-    * (Kitchen kitchen), mas ele está sendo está sendo atribuído na instância que está retornando pelo método merge.
-    * E por isso que Kitchen é que está sendo retornado: */
     @Transactional
-    public Kitchen adicionar(Kitchen kitchen) {
+    public Kitchen salvar(Kitchen kitchen) {
         return manager.merge(kitchen);
     }
 }
--- src/main/java/com/tuyo/tuyofood/jpa/UpdateKitchenMain.java
+++ src/main/java/com/tuyo/tuyofood/jpa/UpdateKitchenMain.java
@@ -6,14 +6,12 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-import java.util.List;
-
 /* 1. Esta classe representa uma Main apenas para testar sem uma Controller.
 *  2. Serve também para testar a inicialização não-web
 *  3. ApplicationContext: é uma interface que gerencia o contexto da aplicação.
 *  4. NONE = determina que aqui não é uma aplicação WEB. */
 
-public class KitchenInclusionMain {
+public class UpdateKitchenMain {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(TuyoFoodApplication.class)
@@ -22,16 +20,10 @@ public class KitchenInclusionMain {
 
         KitchenRegister kitchenRegister = applicationContext.getBean(KitchenRegister.class);
 
-        Kitchen kitchen1 = new Kitchen();
-        kitchen1.setNome("Brasileira");
+        Kitchen kitchen = new Kitchen();
+        kitchen.setId(1L);
+        kitchen.setNome("Chinese");
 
-        Kitchen kitchen2 = new Kitchen();
-        kitchen2.setNome("Japonesa");
-
-        kitchen1 = kitchenRegister.salvar(kitchen1);
-        kitchen2 = kitchenRegister.salvar(kitchen2);
-
-        System.out.printf("%d - %s\n", kitchen1.getId(), kitchen1.getNome());
-        System.out.printf("%d - %s\n", kitchen2.getId(), kitchen2.getNome());
+        kitchenRegister.salvar(kitchen);
     }
 }
