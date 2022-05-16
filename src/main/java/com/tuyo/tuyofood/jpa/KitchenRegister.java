cff295a67681769c1a26df8f5c78b6fda54ad5db
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-16 12:42:58 -0300

    #commit 1 Spring_Data_JPA - inclusao

--- src/main/java/com/tuyo/tuyofood/jpa/KitchenRegister.java
+++ src/main/java/com/tuyo/tuyofood/jpa/KitchenRegister.java
@@ -2,6 +2,7 @@ package com.tuyo.tuyofood.jpa;
 
 import com.tuyo.tuyofood.domain.entity.Kitchen;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -24,4 +25,18 @@ public class KitchenRegister {
         return manager.createQuery("from Kitchen", Kitchen.class)
                 .getResultList();
     }
+
+    /* A anotação @Transactional = representa a transação que precisa ser feita no database.
+    *  1. Inserção direta no database sem JPA: insert into kitchen (nome) values (kitchen = tabela e (nome)= seriam as colunas)
+    *  2. Com JPA: return manager.merge(entity) = colocando a entity, kitchen, no contexto de persistência.
+    *  3. return = representa o retorno da instância persistida.
+    *  4. O método "merge" não altera a instância no parâmetro: quando uma nova cozinha é incluída, um novo "id" é atribuído
+    * no database que está configurado com "auto-incremento". Esse "id" não estará atribuído na instância de Kitchen em
+    * (Kitchen kitchen), mas ele está sendo está sendo atribuído na instância que está retornando pelo método merge.
+    * E por isso que Kitchen é que está sendo retornado: */
+    @Transactional
+    public Kitchen adicionar(Kitchen kitchen) {
+        return manager.merge(kitchen);
+    }
 }
+
