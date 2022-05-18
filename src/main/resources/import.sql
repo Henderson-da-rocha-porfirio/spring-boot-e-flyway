cee92aa4190c4ee98f23851eaeae191cdfa11c2f
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-18 15:47:24 -0300

    #commit 6 Spring_Data_JPA - Repository

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
 insert into kitchen (nome) values ('Tailandesa');
 insert into kitchen (nome) values ('Indiana');
+
+insert into restaurant (nome, taxa_frete) values ('Big Batatas', 18);
+insert into restaurant (nome, taxa_frete) values ('Mannupí', 10);
+insert into restaurant (nome, taxa_frete) values ('Cairpira', 10);
+insert into restaurant (nome, taxa_frete) values ('Bologna Restaurantes', 45);
