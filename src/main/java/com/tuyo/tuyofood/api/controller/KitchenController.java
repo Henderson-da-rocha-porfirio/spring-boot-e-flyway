5f2ce0634041a6887474442a1e71fee2b992cc0f
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-23 17:40:53 -0300

    #commit 5 REST_Singleton_Resource

--- src/main/java/com/tuyo/tuyofood/api/controller/KitchenController.java
+++ src/main/java/com/tuyo/tuyofood/api/controller/KitchenController.java
@@ -3,12 +3,7 @@ package com.tuyo.tuyofood.api.controller;
 import com.tuyo.tuyofood.domain.entity.Kitchen;
 import com.tuyo.tuyofood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +14,25 @@ public class KitchenController {
     @Autowired
     private KitchenRepository kitchenRepository;
 
+    /* Listando Coleções de Cozinha */
     @GetMapping
     public List<Kitchen> listar() {
         return kitchenRepository.listar();
     }
 
+    /* * Buscando Uma (singleton) Cozinha específica
+       * @PathVariable = passagem da variável onde queremos realizar o Bind (ligação).Tipo: /kitchens/{kitchenId}
+       *
+       * Método comum para a realização do Bind:
+
+    @GetMapping("/{kitchenId}")
+    public Kitchen buscar(@PathVariable("kitchenId") Long id) {
+        return kitchenRepository.buscar(id);
+    }*/
+
+    /* Com Bind Automático */
+    @GetMapping("/{kitchenId}")
+    public Kitchen buscar(@PathVariable("kitchenId") Long kitchenId) {
+        return kitchenRepository.buscar(kitchenId);
+    }
 }
