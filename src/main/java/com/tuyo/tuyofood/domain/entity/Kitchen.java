e767035636ee503e95b4a914ae400acbad5429ea
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-24 11:57:04 -0300

    #commit 2 REST_Customizando_Representacoes_JSON_e_XML

--- src/main/java/com/tuyo/tuyofood/domain/entity/Kitchen.java
+++ src/main/java/com/tuyo/tuyofood/domain/entity/Kitchen.java
@@ -1,13 +1,18 @@
 package com.tuyo.tuyofood.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 
-/* Modificando o nome da representação: @JsonProperty */
+/* 1. Modificando o nome da representação sem mudar a propriedade do nome de domínio: @JsonProperty.
+*  2. Ignorando a propriedade: @JsonIgnore
+*  3. Modificando o nome do elemento para XML: @JsonRootName */
+@JsonRootName("kitchen")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -18,6 +23,7 @@ public class Kitchen {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /*@JsonIgnore*/
     @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
