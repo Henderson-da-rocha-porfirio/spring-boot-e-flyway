bd4d2c6f9f3c507bb6ab1177dc2ca26d4aae76ae
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-24 11:47:15 -0300

    #commit 6 REST_Singleton_Resource

--- src/main/java/com/tuyo/tuyofood/domain/entity/Kitchen.java
+++ src/main/java/com/tuyo/tuyofood/domain/entity/Kitchen.java
@@ -1,12 +1,13 @@
 package com.tuyo.tuyofood.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 
+/* Modificando o nome da representação: @JsonProperty */
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -17,6 +18,7 @@ public class Kitchen {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 
