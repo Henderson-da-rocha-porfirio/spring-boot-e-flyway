caa171d9c957dd0a44836c57cb1359368aa822c7
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-31 17:55:20 -0300

    #commit 2 - REST_DOMAIN_SERVICE

---
+++ src/main/java/com/tuyo/tuyofood/domain/service/KitchenRegisterService.java
@@ -0,0 +1,17 @@
+package com.tuyo.tuyofood.domain.service;
+
+import com.tuyo.tuyofood.domain.entity.Kitchen;
+import com.tuyo.tuyofood.domain.repository.KitchenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KitchenRegisterService {
+
+    @Autowired
+    private KitchenRepository kitchenRepository;
+
+    public Kitchen salvar(Kitchen kitchen) {
+        return kitchenRepository.salvar(kitchen);
+    }
+}
