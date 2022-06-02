6873aae5ce5c7142caa9f9b3dcd0c61b42c4af24
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-06-02 11:50:39 -0300

    #commit 3 - REST_DOMAIN_SERVICE_REMOVE

---
+++ src/main/java/com/tuyo/tuyofood/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,6 @@
+package com.tuyo.tuyofood.domain.exception;
+
+public class EntidadeEmUsoException extends Throwable {
+    public EntidadeEmUsoException(String format) {
+    }
+}
--- src/main/java/com/tuyo/tuyofood/domain/service/KitchenRegisterService.java
+++ src/main/java/com/tuyo/tuyofood/domain/service/KitchenRegisterService.java
@@ -1,6 +1,7 @@
 package com.tuyo.tuyofood.domain.service;
 
 import com.tuyo.tuyofood.domain.entity.Kitchen;
+import com.tuyo.tuyofood.domain.exception.EntidadeEmUsoException;
 import com.tuyo.tuyofood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -12,7 +13,12 @@ import org.springframework.stereotype.Service;
 * Ele deve ficar na classe controladora KitchenController.
 *  3. E o tratamento de exceções deve ser feito aqui e não mais em KitchenController.
 *  4. Esta classe não pode ter acesso aquelas classes que estão implementando a API.
-*  5. Sendo uma Classe de Serviço, ela não tem nenhum conhecimento nenhum de protocolo HTTP. */
+*  5. Sendo uma Classe de Serviço, ela não tem nenhum conhecimento nenhum de protocolo HTTP.
+*  6. DataIntegrityViolationException: essa exceção é lançada quando uma kitchen está em uso por um Restaurant.
+* Ou seja, no cadastro de restaurantes tem um restaurant cadastrado para uma determinada kitchen. E estou
+* tentando excluir esta determinada kitchen mas não pode porque tem restaurants nessa determinada kitchen. E
+* por isso não posso excluir e lança essa exceção.
+*  7. EntidadeEmUsoException: é uma exceção de negócio. */
 
 @Service
 public class KitchenRegisterService {
