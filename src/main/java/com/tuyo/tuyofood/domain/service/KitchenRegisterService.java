a54019123f8be6fabd67ce5214254a58b0468fc9
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-06-02 10:21:00 -0300

    #commit 1 - REST_DOMAIN_SERVICE_REMOVE

--- src/main/java/com/tuyo/tuyofood/domain/service/KitchenRegisterService.java
+++ src/main/java/com/tuyo/tuyofood/domain/service/KitchenRegisterService.java
@@ -3,6 +3,8 @@ package com.tuyo.tuyofood.domain.service;
 import com.tuyo.tuyofood.domain.entity.Kitchen;
 import com.tuyo.tuyofood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 /* @Service = Também é um @Component */
@@ -15,5 +17,19 @@ public class KitchenRegisterService {
 
     public Kitchen salvar(Kitchen kitchen) {
         return kitchenRepository.salvar(kitchen);
+    }
+
+    public void excluir(Long kitchenId) {
+        try {
+            kitchenRepository.remover(kitchenId);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de kitchen com código %d", kitchenId));
+
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format("Cozinha de código %d não pode ser removida, pois está em uso", kitchenId));
+        }
     }
 }
