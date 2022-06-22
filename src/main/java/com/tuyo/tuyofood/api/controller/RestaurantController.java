4025ddf3f52c0ce793f8e9bbbbe08737ddd52e24
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-06-21 21:29:05 -0300

    #commit 1 REST_RESOURCES_CITY_AND_STATE

--- src/main/java/com/tuyo/tuyofood/api/controller/RestaurantController.java
+++ src/main/java/com/tuyo/tuyofood/api/controller/RestaurantController.java
@@ -4,12 +4,14 @@ import com.tuyo.tuyofood.domain.entity.Restaurant;
 import com.tuyo.tuyofood.domain.exception.EntidadeNaoEncontradaException;
 import com.tuyo.tuyofood.domain.repository.RestaurantRepository;
 import com.tuyo.tuyofood.domain.service.RestaurantRegisterService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /* <?> = WILDCARD: é um coringa. Alí, no caso, aceita um ResponseEntity com
 * qualquer tipo de corpo. Podendo ser uma String, um Restaurant ou qualquer outro tipo ou coisa. */
@@ -51,5 +53,46 @@ public class RestaurantController {
             return ResponseEntity.badRequest()
                     .body(e.getMessage());
         }
+    }
+
+    @PutMapping("/{restaurantId}")
+    public ResponseEntity<?> atualizar(@PathVariable Long restaurantId,
+                                       @RequestBody Restaurant restaurant) {
+        try {
+            Restaurant restaurantAtual = restaurantRepository.buscar(restaurantId);
+
+            if (restaurantAtual != null) {
+                BeanUtils.copyProperties(restaurant, restaurantAtual, "id");
+
+                restaurantAtual = restaurantRegisterService.salvar(restaurantAtual);
+                return ResponseEntity.ok(restaurantAtual);
+            }
+
+            return ResponseEntity.notFound().build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/{restaurantId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long restaurantId,
+                                              @RequestBody Map<String, Object> campos) {
+        Restaurant restaurantAtual = restaurantRepository.buscar(restaurantId);
+
+        if (restaurantAtual == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        merge(campos, restaurantAtual);
+
+        return atualizar(restaurantId, restaurantAtual);
+    }
+
+    private void merge(Map<String, Object> camposOrigem, Restaurant restaurantDestino) {
+        camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            System.out.println(nomePropriedade + " = " + valorPropriedade);
+        });
     }
 }
