80e431f0c809f0232eb7608666ab3cd709a8d626
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-06-27 22:08:35 -0300

    #commit 4 SPRING_DATA_JPA_Repository

--- src/main/java/com/tuyo/tuyofood/domain/repository/KitchenRepository.java
+++ src/main/java/com/tuyo/tuyofood/domain/repository/KitchenRepository.java
@@ -1,28 +1,13 @@
 package com.tuyo.tuyofood.domain.repository;
 
 import com.tuyo.tuyofood.domain.entity.Kitchen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-/* 1. Uma Interface Repository ( Repositório Orientado a persistência ):
- *  a. Pertence ao Domínio da Aplicação:
- *  b. Não deve ter muitos detalhes técnicos
- *  c. Não informa nada sobre o mecanismo de persistência dos dados
- *  d. Por isso é colocada dentro de Domain
- *  e. É preciso criar uma classe para implementar esta classe.
- *  f. Por padrão, ele deve imitar uma Coleção: add, get e etc...
- *  g. Não se cria um repositório por tabela e/ou por entidade.
- *  h. Um repository é por agregado (conjunto de agregado/aggregate)
- *  i. Não criar repositórios para entidades que não são "aggregate root".
- *  2. Pensando na Camada de Negócio de um Delivery de Comida: o que um repositório de cozinha tem que ter:
- *  a. Listar Cozinhas
- *  b. Buscar Cozinhas pelo ID
- *  c. Salvar uma Cozinha
- *  d. Remover uma Cozinha
- *  e. Poderia ter outros métodos específicos
- *  f. List<Kitchen> consultarPorNome(String nome): é uma nova assinatura de método */
+@Repository
+public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
 
-public interface KitchenRepository {
-
-    List<Kitchen> consultarPorNome(String nome);
+    /*List<Kitchen> consultarPorNome(String nome);*/
 }
