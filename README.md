69d1cfad5716c5f4c15e29ab0d1ae7b20c19bfa4
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-07-01 16:07:03 -0300

    #commit 1 SPRING_DATA_JPA_CONSULTAS_COM_@QUERY

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SPRING_DATA_JPA_Repository
+# SPRING_DATA_JPA_CONSULTAS_COM_@QUERY
 
 ## 1. JPA e Hibernate:
 ### — A. Uma Interface Repository ( Repositório Orientado a persistência ):
--- src/main/java/com/tuyo/tuyofood/domain/repository/RestaurantRepository.java
+++ src/main/java/com/tuyo/tuyofood/domain/repository/RestaurantRepository.java
@@ -12,7 +12,11 @@ import java.util.Optional;
 
 /* 1. nome: passado no parâmetro deve fazer o bind com %:nome%
 *  2. @Param("id"): faz o bind de kitchen passado no parâmetro com :id
-*  3. @Query: torna possível o uso do JPQL   */
+*  3. @Query: torna possível o uso do JPQL
+*  4. Consultas:
+*  a. @Query: usando JPQL
+*  b. Find: usando prefixos e flags
+*  c. Externas: META-INF/orm.xml   */
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
