a65d6509cbd09d7dca1baa0c155135baf32e7bbb
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-07-20 14:14:58 -0300

    #commit 1 Controller_-_atualizando_dados_sem_apagar_os_antigos

--- src/main/java/com/tuyo/tuyofood/domain/entity/Restaurant.java
+++ src/main/java/com/tuyo/tuyofood/domain/entity/Restaurant.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tuyo.tuyofood.domain.entity.embeddables.Address;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +26,20 @@ import java.util.List;
 * remover ou comentar o @JsonIgnore. Mas no DataBase continua do mesmo jeito.
 *  8. *IMPORTANTE*: Ambas propriedades: tanto address quanto paymentForms precisam ser adicionadas no parâmetro
 * do método atualizar de RestaurantController para que ao adicionarem novos dados,
-* não sejam deletados os antigos. */
+* não sejam deletados os antigos.
+*  9. LocalDateTime: Representa uma data/hora sem fuso-horário(timestamp) definido
+*  10. nullable = false: mostra que as propriedades são obrigatórias. E ainda as usamos porque ainda estamos
+* utilizando o "generate ddl" para gerar o banco de dados automaticamente. E isso também faz com que ele crie a
+* uma coluna "notNull".
+*  11. Generate DDL: geração do banco de dados automaticamente. A configuração é feita no application.properties
+*  12. Datas: A lógica é a seguinte, quando salvar um restaurant, é preciso obter uma nova instância de
+* LocalDateTime que represente aquele exato momento(data/hora) que precisa ser atribuída.
+*  13. dataCadastro: sempre que criar um novo Restaurant, atribui um dataCadastro.
+*  14. dataAtualizacao: é atribuição de data/hora a partir do dataCadastro. Ou seja, não é a primeira data/hora
+* mas as últimas data/hora.
+*  15. As ações acima poderiam ser feitas na classe service no sentido de ter a opção de quando usar uma ou outra
+* função.
+*  16. @CreationTimestamp:   */
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -42,13 +58,23 @@ public class Restaurant {
     private BigDecimal taxaFrete;
 
  // @JsonIgnore
-    @ManyToOne
+@ManyToOne
     @JoinColumn(name =  "kitchen_id", nullable = false)
     private Kitchen kitchen;
 
     @JsonIgnore
     @Embedded
     private Address address;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataAtualizacao;
 
     @JsonIgnore
     @ManyToMany
