8fa6dffd6b5e9001b0565f0dd1e33ddcc3d126fc
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-07-29 12:01:37 -0300

    COMMIT 1 master

---
+++ src/main/java/com/tuyo/MigrationflywayspringApplication.java
@@ -0,0 +1,13 @@
+package com.tuyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationflywayspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationflywayspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuyo/MigrationflywayspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationflywayspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
